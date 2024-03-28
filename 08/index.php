ea56abfc9a58c320b13801f287f1768c5933e34a
Guilherme <guilherme94siqueira@gmail.com>
2024-03-27 22:15:30 -0300

    08 - estruturas de repetição

---
+++ 08/index.php
@@ -0,0 +1,34 @@
+<?php
+    // ESTRUTURAS DE REPETIÇÃO EM PHP
+
+    // Exemplo 1 : FOR
+   echo "Exemplo 1: For <br>";
+   for($i = 1;$i <= 5 ;$i++) {
+       echo "Numero $i <br>";
+   }
+
+   // Exemplo 2: While
+   echo "<br>Exemplo 2: while <br>";
+   
+   $contador = 1;
+   while($contador <= 5) {
+    echo "Numero $contador <br>";
+    $contador++;
+   }
+
+   // Exemplo 3: do-While
+   echo "<br>Exemplo 3: DO_WHILE <br>";
+   $cont_do = 1;
+   // garante que a condição seja execultada ao menos uma vez
+   do {
+    echo "Numero: $cont_do <br>";
+    $cont_do++;
+   } while ($cont_do <= 5);
+
+   // EXEMPLO 4: FOREACH
+   echo "<br>Exemplo 4: FOREACH <br>";
+   $array_vogais = ["a","e","i","o","u"];
+   foreach($array_vogais as $vogal) {
+    echo "Vogal $vogal <br>"; 
+   }
+?>
