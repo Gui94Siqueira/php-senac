3e4f243595cd83a02172aa21fe851c14e72a79b0
Guilherme <guilherme94siqueira@gmail.com>
2024-04-11 21:41:36 -0300

    corringindo erros

--- 16/exibeSolucao.php
+++ 16/exibeSolucao.php
@@ -3,4 +3,5 @@ require_once 'solucao.php';
 $repository = new DatabaseRepository();
 
 print_r($repository->getAllItems());
+
 ?>
--- 16/solucao.php
+++ 16/solucao.php
@@ -2,18 +2,18 @@
 
 class DatabaseRepository {
     private $server = 'localhost';
-    private $username = 'root';
-    private $password = '';
+    private $username = 'usuario_lista_compras';
+    private $password = 'Senha@123';
     private $database = 'lista_compras';
     private $connection;
-
-    public function __constructor() {
+    
+    public function __construct() {
         $this->connection = new mysqli(
             $this->server, 
             $this->username, 
             $this->password, 
             $this->database);
-            
+
         if($this->connection->connect_errno) {
             die("Erro na conexão: " . $this->connection->connect_error);
         }
@@ -21,19 +21,34 @@ class DatabaseRepository {
 
     public function getAllItems() {
         $sql = "SELECT * FROM itens_compra";
-        return $this->connection->query($sql);
-        //$stmt->execute();
-        //$result = $stmt->get_result();
-        //$items = $result->fetch_all();
-        //$stmt->close();
-        //return $items;
+        $stmt = $this->connection->prepare($sql);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $items = $result->fetch_all();
+        $stmt->close();
+        return $items;
     }
 
+    public function addItem($nome_produto, $quantidade) {
+        $sql = "INSERT INTO itens_compra (nome_produto, quantidade) VALUES (?, ?)";
+        $stmt = $this->connection->prepare($sql);
+        $stmt->bind_param("si", $nome_produto, $quantidade);
+        $success = $stmt->execute();
+        $stmt->close();
+        return $success;
+    }
 
+    public function updateItem($id, $nome_produto, $quantidade) {
+        
+    }
+
+    public function deleteItem() {
+
+    }
+
+    public function __destruct() {
+        $this->connection->close();
+    }
 }
-
-$repository = new DatabaseRepository();
-
-print_r($repository->getAllItems());
 
 ?>
