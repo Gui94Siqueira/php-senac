ac1a44890fa399dd043f001d82a58daaaeef967e
Guilherme <guilherme94siqueira@gmail.com>
2024-04-17 20:13:41 -0300

    update aula 17

---
+++ 18/database/DatabaseRepository.php
@@ -0,0 +1,15 @@
+<?php
+    class DatabaseRepository {
+        private static $server = 'localhost';
+        private static $username ='root';
+        private static $password = '';
+        private static $database = 'banco_contas';
+
+        public static function connect() {
+            return new mysqli(self::$server, self::$username, self::$password, self::$database);
+        }
+
+        
+    }
+
+?>
