864972bf632aaacad5d603471832d21056ea5b0a
GUILHERME SANTOS SIQUEIRA <guilherme.ssiqueira4@senacsp.edu.br>
2024-04-18 20:10:30 -0300

    aula 18 - contas api listar contas

--- 18/api/conta_api.php
+++ 18/api/conta_api.php
@@ -1,5 +1,20 @@
 <?php
     require_once '../database/ContaRepository.php';
 
+    $action = $_GET['action'];
+    
+    switch($action) {
+        case 'list':
+            listarContas();
+            break;
+        default:
+        http_response_code(400);
+        echo json_encode(['Error' => 'Ação inválida']);
+    }
+
+    function listarContas() {
+        $contas = ContaRepository::getAllContas();
+        echo json_encode($contas);
+    }
     
 ?>
