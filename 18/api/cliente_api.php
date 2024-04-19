2b2e184668dfda1d3afbb8ff9bf76889229228fe
Guilherme <guilherme94siqueira@gmail.com>
2024-04-18 22:07:24 -0300

    aula 18 cliente repository functions

--- 18/api/cliente_api.php
+++ 18/api/cliente_api.php
@@ -7,6 +7,18 @@
         case 'list':
             listarClientes();
             break;
+        case 'buscar':
+            buscarClientePorId();
+            break;
+        case 'cadastrar':
+            cadastrasCliente();
+            break;
+        case 'atualizar':
+            atualizarCliente();
+            break;
+        case 'excluir':
+            excluirCliente();
+            break;
         default:
         http_response_code(400); // Requisição Inválida
         echo json_encode(['error' => 'Ação inválida']);
@@ -16,4 +28,32 @@
         $cliente = ClienteRepository::getAllClietes():
         echo json_encode($cliente);
     }
+
+    function buscarClientePorId($id) {
+        $cliente = ClienteRepository::getClienteById($id);
+        echo json_encode($cliente);
+    }
+
+    function cadastrasCliente() {
+
+    }
+
+    function inserirCliente() {
+
+    }
+
+    function atualizarCliente() {
+
+    }
+
+    function excluirCliente($id) {
+
+    }
+
+    // CREATE Read update delete
+    // Create = POST(INSERT)
+    // Read = GET(SELECT)
+    // UPDATE = 
+    // DELETE = 
+
 ?>
